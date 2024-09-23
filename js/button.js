fd70c5b488296b94244bc48160e2f5b8917f6219
waridabd <waridabd20@gmail.com>
2024-09-23 22:11:05 +0600

    third commit

---
+++ js/button.js
@@ -0,0 +1,4 @@
+document.getElementById('donate-btn').addEventListener('click',function(){
+    document.getElementById('donation-div') .classList.remove('hidden');
+    
+});
--- js/donation.js
+++ js/donation.js
@@ -1,3 +1,62 @@
-document.getElementById('donationBtn').addEventListener('click',function(){
-    console.log('ami');
-})
+document.getElementById('donate-flood-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    const donateAmount = document.getElementById('donate-money').value;
+    if(donateAmount>0){
+      const floodDonation=document.getElementById('flood-donation') .innerText;
+      const floodMoney=parseFloat(floodDonation);
+      const donateMoney =parseFloat(donateAmount);
+     
+      const newBalance=donateMoney+floodMoney;
+      console.log(newBalance);
+      document.getElementById('flood-donation').innerText=newBalance;
+      const totalAmount =document.getElementById('total-amount').innerText;
+      const newAmount=parseFloat(totalAmount);
+      const newTotal=newAmount-donateMoney;
+      document.getElementById('total-amount').innerText=newTotal;
+    }
+    else{
+      alert("Invalid Amount");
+    }
+});
+
+document.getElementById('donate-feni-btn').addEventListener('click',function(event){
+  event.preventDefault();
+  const donateAmount = document.getElementById('donate-money1').value;
+  if(donateAmount>0){
+    const floodDonation=document.getElementById('feni-donation') .innerText;
+    const floodMoney=parseFloat(floodDonation);
+    const donateMoney =parseFloat(donateAmount);
+   
+    const newBalance=donateMoney+floodMoney;
+    console.log(newBalance);
+    document.getElementById('feni-donation').innerText=newBalance;
+    const totalAmount =document.getElementById('total-amount').innerText;
+    const newAmount=parseFloat(totalAmount);
+    const newTotal=newAmount-donateMoney;
+    document.getElementById('total-amount').innerText=newTotal;
+  }
+  else{
+    alert("Invalid Amount");
+  }
+});
+
+document.getElementById('donate-quota-btn').addEventListener('click',function(event){
+  event.preventDefault();
+  const donateAmount = document.getElementById('quota-money').value;
+  if(donateAmount>0){
+    const floodDonation=document.getElementById('quota-donation') .innerText;
+    const floodMoney=parseFloat(floodDonation);
+    const donateMoney =parseFloat(donateAmount);
+   
+    const newBalance=donateMoney+floodMoney;
+    console.log(newBalance);
+    document.getElementById('quota-donation').innerText=newBalance;
+    const totalAmount =document.getElementById('total-amount').innerText;
+    const newAmount=parseFloat(totalAmount);
+    const newTotal=newAmount-donateMoney;
+    document.getElementById('total-amount').innerText=newTotal;
+  }
+  else{
+    alert("Invalid Amount");
+  }
+});
